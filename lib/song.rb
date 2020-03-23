f5dc8f2415b8e6f83e68bfaa80b116f66a2e4776
Matt Kuras <mattkuras@gmail.com>
2020-03-23 19:54:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -62,7 +62,7 @@ class Song
     artist = Artist.find_or_create_by_name(artist)
     genre = Genre.find_or_create_by_name(genre)
     new(song, artist, genre)
-    binding.pry 
+    #binding.pry 
   end
   
 end
